40af15869ab4692461b776dd890f244935143124
praveenpr1998 <48721940+praveenpr1998@users.noreply.github.com>
2020-04-20 12:55:40 +0530

    Update HomeScreen.js

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,51 +1,57 @@
 import React, { Component } from 'react';
 import { Image, View, Text, AsyncStorage,StyleSheet } from 'react-native';
+import { StackActions } from '@react-navigation/native';
+import { NavigationActions } from 'react-navigation';
 import AnimatedLoader from "react-native-animated-loader";
 
-export default class HomeScreen extends Component {
-    state={
-        visible:true
-    };
-
-    // componentDidMount Life-cycle method
-    async componentDidMount(){
-        setTimeout (
-            async ()=>{
-                    if(await AsyncStorage.getItem("userid")===null){
-                        this.setState({visible:false});
-                        this.props.navigation.navigate('Login');
-                    }
-                    else{
-                        this.props.navigation.navigate('Homee');
-                        this.setState({visible:false})
-                    }
-                },2*1000
-        );
-    }
-
-    render() {
-        return (
-            <View style={{flex:1,backgroundColor:"black",alignItems:'center'}}>
-                <Image
-                    style={{width: 150, height: 158,marginTop:250}}
-                    source={{uri:"https://graphicriver.img.customer.envatousercontent.com/files/270440720/CartoonDogPointer%20p.jpg?auto=compress%2Cformat&q=80&fit=crop&crop=top&max-h=8000&max-w=590&s=d7ccf47eef9f9a8f679c134cc70bffa5"}} />
-                <Text style={{color:"white",fontSize:30,marginTop:0}}>E-Cart</Text>
-
-                <AnimatedLoader
-                    visible={this.state.visible}
-                    source={require("../assets/11625-typing-indicator.json")}
-                    animationStyle={styles.lottie}
-                    speed={1}
-                />
-            </View>
-        )
-    }
+export default class Homescreen extends Component {
+  state={visible:true}
+async componentDidMount(){
+    setTimeout (
+      async ()=>{
+        if(await AsyncStorage.getItem("userid")===null){
+          this.setState({visible:false})
+        this.props.navigation.navigate('Login')
+      }
+        else{
+          this.props.navigation.navigate('Homee')  
+          this.setState({visible:false})        }
+      },2*1000
+    );
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image
+              style={styles.image}
+              source={{uri:"https://graphicriver.img.customer.envatousercontent.com/files/270440720/CartoonDogPointer%20p.jpg?auto=compress%2Cformat&q=80&fit=crop&crop=top&max-h=8000&max-w=590&s=d7ccf47eef9f9a8f679c134cc70bffa5"}} />
+        <Text style={styles.text}>E-Cart</Text>
+        <AnimatedLoader
+        visible={this.state.visible}
+        source={require("../assets/11625-typing-indicator.json")}
+        animationStyle={styles.lottie}
+        speed={1}
+      />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
+  container:{
+    marginTop:10,width: 110, height: 98
+  },
+  text:{
+    color:"white",fontSize:30,marginTop:0
+  },
+  image:{
+    width: 150, height: 158,marginTop:250
+  },
     lottie: {
-        width: 100,
-        height: 100,
-        marginTop:150,
-    },
+    width: 100,
+    height: 100,
+    marginTop:150,
+    
+  }
 });
