a3e0a8af3bbea96b0cd19c4c593964744876d14d
praveenpr1998 <48721940+praveenpr1998@users.noreply.github.com>
2020-04-20 12:52:07 +0530

    Update DrawerNavigator.js

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -34,86 +34,93 @@ async function findname (){
         });
 }
 
+
 const DrawerWithLogoutButton=(props)=>{
     {findname()}
     return(
         <ScrollView contentContainerStyle={{flex: 1,  flexDirection: 'column', justifyContent: 'space-between' }}>
-            <SafeAreaView forceInset={{ top: 'always', horizontal: 'never' }}>
-                <View style={{height:130,paddingLeft:20,paddingTop:20,flexDirection:'row',backgroundColor:'#d3f4ff'}}>
-                    <Avatar size="large" rounded icon={{ name: 'home' }} />
-                    <Text style={styles.username}>{user}</Text>
-                    <TouchableOpacity onPress={()=>{props.navigation.closeDrawer()}} style={{paddingLeft:40,paddingBottom:40}}>
-                        <AntDesign name="closesquare" size={32}   color="black" />
-                    </TouchableOpacity>
-                </View>
-                <TouchableOpacity style={{paddingTop:30}} onPress={()=>{props.navigation.navigate('Homee')}}>
-                    <Text style={{fontSize:20,paddingLeft:20}}>Home</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={{paddingTop:30}} onPress={()=>{props.navigation.navigate('Cartt')}}>
-                    <Text style={{fontSize:20,paddingLeft:20}}>Cart</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={{paddingTop:30}} onPress={()=>{props.navigation.navigate('MyOrders')}}>
-                    <Text style={{fontSize:20,paddingLeft:20}}>MyOrders</Text>
-                </TouchableOpacity>
-            </SafeAreaView>
-            <TouchableOpacity onPress={()=>{AsyncStorage.removeItem("userid");props.navigation.dispatch(resetAction);}}>
-                <View style={styles.item}>
-                    <Text style={styles.label}>Logoutt</Text>
-                    <MaterialCommunityIcons name="logout" size={32} style={{paddingLeft:20}}  color="black" />
-                </View>
+          <SafeAreaView forceInset={{ top: 'always', horizontal: 'never' }}>
+            <View style={styles.viewbox}>
+              <Avatar size="large" rounded icon={{ name: 'home' }} />
+              <Text style={styles.username}>{user}</Text>
+              <TouchableOpacity onPress={()=>{props.navigation.closeDrawer()}} style={styles.drawerback}>           
+                   <AntDesign name="closesquare" size={32}   color="black" />
+               </TouchableOpacity>
+            </View>
+            <TouchableOpacity style={styles.buttonOpac} onPress={()=>{props.navigation.navigate('Homee')}}>
+              <Text style={styles.drawertext}>Home</Text>
             </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonOpac} onPress={()=>{props.navigation.navigate('Cartt')}}>
+              <Text style={styles.drawertext}>Cart</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.buttonOpac} onPress={()=>{props.navigation.navigate('MyOrders')}}>
+              <Text style={styles.drawertext}>MyOrders</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+          <TouchableOpacity onPress={()=>{AsyncStorage.removeItem("userid");props.navigation.dispatch(resetAction);}}>
+            <View style={styles.item}>
+              <Text style={styles.label}>Logoutt</Text>
+              <MaterialCommunityIcons name="logout" size={32} style={styles.logoutbutton}  color="black" />
+            </View>
+          </TouchableOpacity>
       </ScrollView>
     )
-};
+}
 
 const DrawerNavigation =createDrawerNavigator({
-    Homee:{
-        screen:Home,
-    },
-    Login:{
-        screen:Login,
-    },
-    Cartt:{
-        screen:cartPage,
-    },
-    MyOrders:{
-        screen:MyOrders,
-    }
+  Homee:{
+    screen:Home,
+  },
+  Login:{
+    screen:Login,
+  },
+  Cartt:{
+    screen:cartPage,
+  },
+  MyOrders:{
+    screen:MyOrders,
+  }
 },
   {
-      contentComponent:DrawerWithLogoutButton,
-  });
+    contentComponent:DrawerWithLogoutButton,
+    });
 
-const styles = StyleSheet.create({
-    item: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    label: {
-        paddingRight:20,
-        margin: 16,
-        fontSize:20,
-        fontWeight: 'bold',
-        color: 'rgba(0, 0, 0, .87)',
-    },
-    iconContainer: {
-        marginHorizontal: 16,
-        width: 24,
-        alignItems: 'center',
-    },
-    icon: {
-        width: 24,
-        height: 24,
-    },
-    username:{
-        fontSize:25,
-        fontFamily:'Teko-Bold',
-        paddingLeft:20,
-        paddingTop:25,
-        alignContent:'center',
-        color:"#ffaaa5"
-    }
-});
-
-const DrawerNav = createAppContainer(DrawerNavigation);
-export default DrawerNav;
+    const DrawerNav = createAppContainer(DrawerNavigation);
+    const styles = StyleSheet.create({
+        item: {
+            flexDirection: 'row',
+            alignItems: 'center',
+          },
+          logoutbutton:{paddingLeft:20},
+          label: {
+            paddingRight:20,
+            margin: 16,
+            fontSize:20,
+            fontWeight: 'bold',
+            color: 'rgba(0, 0, 0, .87)',
+          },
+          iconContainer: {
+            marginHorizontal: 16,
+            width: 24,
+            alignItems: 'center',
+          },
+          icon: {
+            width: 24,
+            height: 24,
+          },
+          username:{
+            fontSize:25,
+            fontFamily:'Teko-Bold',
+            paddingLeft:20,
+            paddingTop:25,
+            alignContent:'center',
+            color:"#ffaaa5"
+          },
+          drawertext:{fontSize:20,paddingLeft:20},
+          drawerback:{paddingLeft:40,paddingBottom:40},
+          viewbox:{height:130,paddingLeft:20,paddingTop:20,flexDirection:'row',backgroundColor:'#d3f4ff'},
+          buttonOpac:{
+            paddingTop:30
+          }
+      });
+    export default DrawerNav;
